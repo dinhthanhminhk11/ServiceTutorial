b65bc846fdc716e5c5baafb1a05d898c71d24d2b
minhdinh <minhdinhthanh@vccorp.vn>
2023-03-01 15:44:58 +0700

    push

--- app/src/main/java/com/example/servicetutorial/MainActivity.java
+++ app/src/main/java/com/example/servicetutorial/MainActivity.java
@@ -1,17 +1,69 @@
 package com.example.servicetutorial;
 
+import static com.example.servicetutorial.MyService.ACTION_CLOSE;
+import static com.example.servicetutorial.MyService.ACTION_PAUSE;
+import static com.example.servicetutorial.MyService.ACTION_RESUME;
+import static com.example.servicetutorial.MyService.ACTION_START;
+
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.example.servicetutorial.model.Song;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText text;
     private Button start;
-    private Button stop;
+    private Button stops;
+    private LinearLayout contentLayoutMusic;
+    private ImageView image;
+    private TextView title;
+    private ImageView play;
+    private ImageView close;
+
+    private Song song;
+    private boolean isPlay;
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // nơi nhận data
+            Bundle bundle = intent.getExtras();
+            if (bundle == null) {
+                return;
+            }
+            song = (Song) bundle.get("objectSong");
+            isPlay = bundle.getBoolean("status");
+            int action = bundle.getInt("action_music");
+            checkUI(action);
+        }
+    };
+
+    private void checkUI(int action) {
+        switch (action) {
+            case ACTION_PAUSE:
+                contentLayoutMusic.setVisibility(View.VISIBLE);
+                break;
+            case ACTION_RESUME:
+                contentLayoutMusic.setVisibility(View.VISIBLE);
+                break;
+            case ACTION_CLOSE:
+                contentLayoutMusic.setVisibility(View.GONE);
+                break;
+            case ACTION_START:
+                contentLayoutMusic.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -19,19 +71,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = (EditText) findViewById(R.id.text);
+        /// đăng kí lắng nghe BroadcastReceiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, new IntentFilter("send_data_to_activity"));
+
         start = (Button) findViewById(R.id.start);
-        stop = (Button) findViewById(R.id.stop);
+        stops = (Button) findViewById(R.id.stops);
+        contentLayoutMusic = (LinearLayout) findViewById(R.id.contentLayoutMusic);
+        image = (ImageView) findViewById(R.id.image);
+        title = (TextView) findViewById(R.id.title);
+        play = (ImageView) findViewById(R.id.play);
+        close = (ImageView) findViewById(R.id.close);
 
         start.setOnClickListener(v -> {
+            Song song = new Song("Title", "Minh", R.drawable.musicimage, R.raw.filemusic);
+
             Intent intent = new Intent(this, MyService.class);
-            intent.putExtra("text", text.getText().toString());
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("song", song);
+            intent.putExtras(bundle);
+
             startService(intent);
         });
 
-        stop.setOnClickListener(v -> {
+        stops.setOnClickListener(v -> {
             stopService(new Intent(this, MyService.class));
         });
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+    }
+
+    private void sendDataToService(int action) { //send đén onStartCommand
+        Intent intent = new Intent(this, MyService.class);
+        intent.putExtra("action_music", action);
+        startService(intent);
     }
 }
--- app/src/main/java/com/example/servicetutorial/MyService.java
+++ app/src/main/java/com/example/servicetutorial/MyService.java
@@ -2,17 +2,60 @@ package com.example.servicetutorial;
 
 import static com.example.servicetutorial.MyApplication.CHANNEL_ID;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.example.servicetutorial.model.Song;
 
 public class MyService extends Service {
+    private Song mSong;
+    private ImageView image;
+    private TextView title;
+    private ImageView play;
+    private ImageView close;
+    private boolean isPlaying;
+    public static final int ACTION_PAUSE = 1;
+    public static final int ACTION_RESUME = 2;
+    public static final int ACTION_CLOSE = 3;
+    public static final int ACTION_START = 4;
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            Song song = (Song) bundle.get("song");
+            mSong = song;
+            sendNotification(song);
+            startMusic(song);
+        }
+
+        int actionMusic = intent.getIntExtra("action_music", 0);
+        handleActionMusic(actionMusic);
+        return START_NOT_STICKY;
+    }
 
     @Override
     public void onCreate() {
@@ -20,36 +63,105 @@ public class MyService extends Service {
         Log.e("Minh", "onCreate");
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    // nhảy vào đây khi gọi startService hoặc stopService hoặc gọi hàm stopSelf
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.e("Minh", "onStartCommand");
-        Log.e("Minh", "data " + intent.getStringExtra("text"));
-        senNotification(intent.getStringExtra("text"));
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    private void senNotification(String text) {
-        Intent intent = new Intent(this, MainActivity.class);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle("Title Notition service").setContentText("text").setSmallIcon(R.mipmap.ic_launcher).setContentIntent(pendingIntent).build();
-
-        //không có hàm này thì sẽ chạy 1 phút rồi tự ngừng
-        startForeground(1, notification);
-
-    }
-
     @Override
     public void onDestroy() {
-        Log.e("Minh", "onDestroy");
         super.onDestroy();
+        Log.e("Minh", "onDestroy");
+
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    private void sendNotification(Song song) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        if (song != null) {
+
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), song.getImage());
+
+            @SuppressLint("RemoteViewLayout") RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.layoutnoti);
+            remoteViews.setTextViewText(R.id.title, song.getTitle());
+            remoteViews.setImageViewBitmap(R.id.image, bitmap);
+            remoteViews.setImageViewResource(R.id.play, R.drawable.baseline_pause_24);
+
+            if (isPlaying) {
+                remoteViews.setOnClickPendingIntent(R.id.play, getPendingIntent(this, ACTION_PAUSE));
+                remoteViews.setImageViewResource(R.id.play, R.drawable.baseline_pause_24);
+            } else {
+                remoteViews.setOnClickPendingIntent(R.id.play, getPendingIntent(this, ACTION_RESUME));
+                remoteViews.setImageViewResource(R.id.play, R.drawable.baseline_play_arrow_24);
+            }
+
+            remoteViews.setOnClickPendingIntent(R.id.close, getPendingIntent(this, ACTION_CLOSE));
+
+            // từ phiên bản android 8.1 bắt buộc phải có noti
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID).setSmallIcon(R.drawable.musicimage).setContentIntent(pendingIntent).setCustomContentView(remoteViews).setSound(null).build();
+            // không có noti chỉ chạy đc 1 phút
+            startForeground(1, notification);
+        }
+    }
+
+    private PendingIntent getPendingIntent(Context context, int actionPause) {
+        Intent intent = new Intent(this, MyReceiver.class);
+        intent.putExtra("music", actionPause);
+        // truyền dữ liệu
+        return PendingIntent.getBroadcast(context.getApplicationContext(), actionPause, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private void startMusic(Song song) {
+        senActionToActivity(ACTION_START);
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), song.getResource());
+        }
+        isPlaying = true;
+        mediaPlayer.start();
+    }
+
+    private void pause() {
+        senActionToActivity(ACTION_PAUSE);
+        if (mediaPlayer != null && isPlaying) {
+            mediaPlayer.pause();
+            isPlaying = false;
+        }
+    }
+
+    private void resume() {
+        senActionToActivity(ACTION_RESUME);
+        if (mediaPlayer != null && isPlaying) {
+            mediaPlayer.pause();
+            isPlaying = true;
+        }
+    }
+
+    private void handleActionMusic(int action) {
+        switch (action) {
+            case ACTION_PAUSE:
+                pause();
+                break;
+            case ACTION_RESUME:
+                resume();
+                break;
+            case ACTION_CLOSE:
+                stopSelf();
+                senActionToActivity(ACTION_CLOSE);
+                break;
+        }
+    }
+
+    private void senActionToActivity(int action) {
+        Intent intent = new Intent("send_data_to_activity");
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("objectSong", mSong);
+        bundle.putBoolean("status", isPlaying);
+        bundle.putInt("action_music", action);
+        intent.putExtras(bundle);
+
+        // gửi data đi BroadcastReceiver
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 }
