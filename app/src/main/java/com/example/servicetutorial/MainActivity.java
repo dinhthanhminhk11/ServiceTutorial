9cd6b9ccd4649195fc55ce6e32ef2789522abcd4
minhdinh <minhdinhthanh@vccorp.vn>
2023-03-01 17:20:23 +0700

    push

--- app/src/main/java/com/example/servicetutorial/MainActivity.java
+++ app/src/main/java/com/example/servicetutorial/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.servicetutorial;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private Button start;
+    private Button stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.start);
+        stop = (Button) findViewById(R.id.stop);
+
+        start.setOnClickListener(v -> {
+
+        });
+
+        stop.setOnClickListener(v -> {
+
+        });
+
     }
 }
