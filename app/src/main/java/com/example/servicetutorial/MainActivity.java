1cf52f3b382e3952027e0d5d4ee0dc4b9bbfd366
minhdinh <minhdinhthanh@vccorp.vn>
2023-03-02 10:21:32 +0700

    push

--- app/src/main/java/com/example/servicetutorial/MainActivity.java
+++ app/src/main/java/com/example/servicetutorial/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.servicetutorial;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +16,33 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     private Button start;
     private Button stop;
+
+    private Messenger messenger;
+    private boolean isCheckConnect;
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            messenger = new Messenger(iBinder);
+            isCheckConnect = true;
+            sendMessagePlayMusic();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isCheckConnect = false;
+            messenger = null;
+        }
+    };
+
+    private void sendMessagePlayMusic() {
+        Message message = Message.obtain(null, MusicService.MSG_SAY_HELLO, 0, 0);
+        try {
+            // send dữ liệu
+            messenger.send(message);
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +53,15 @@ public class MainActivity extends AppCompatActivity {
         stop = (Button) findViewById(R.id.stop);
 
         start.setOnClickListener(v -> {
-
+            Intent intent = new Intent(this, MusicService.class);
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
         });
 
         stop.setOnClickListener(v -> {
-
+            if (isCheckConnect) {
+                unbindService(serviceConnection);
+                isCheckConnect = false;
+            }
         });
 
     }
--- app/src/main/java/com/example/servicetutorial/MusicService.java
+++ app/src/main/java/com/example/servicetutorial/MusicService.java
@@ -1,15 +1,74 @@
 package com.example.servicetutorial;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MusicService extends Service {
+    public static final int MSG_SAY_HELLO = 1;
+    private MediaPlayer mediaPlayer;
+    private Messenger messenger;
+
+    public class MyHandler extends Handler {
+        private Context context;
+
+        public MyHandler(Context context) {
+            this.context = context.getApplicationContext();
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            // nhận dữ liệu
+            if (msg.what == MSG_SAY_HELLO) {
+                startMusic();
+            }
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e("Minh_Log", "onCreate");
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Log.e("Minh_Log", "onBind");
+        messenger = new Messenger(new MyHandler(this));
+        return messenger.getBinder();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.e("Minh_Log", "onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e("Minh_Log", "onDestroy");
+        if(mediaPlayer !=null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    private void startMusic() {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.filemusic);
+        }
+        mediaPlayer.start();
     }
 }
